633db7aa7e16e512f2b69ad58aec9ae7e24325b7
rumi <myunpe2@gmail.com>
2014-07-05 17:55:28 +0900

    GameTitleシーンにGameStartButtonを設置

---
+++ UnityProject/Assets/UnityChan/Scripts/GameTitle/UIObjectController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIObjectController : MonoBehaviour {
+	
+	public GUISkin guiSkin;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frameZ:\Unity\UnityChan
+	void Update () {
+	
+	}
+
+
+	void OnGUI(){
+		if (guiSkin == null)
+			return;
+
+		GUI.skin = guiSkin;
+
+		int buttonX = Screen.width / 2;
+		int buttonY = Screen.height / 2;
+		if (GUI.Button (new Rect (buttonX, buttonY, 200, 100), "GameStart")) {
+			Debug.Log("GameStart");
+		}
+	}
+}
