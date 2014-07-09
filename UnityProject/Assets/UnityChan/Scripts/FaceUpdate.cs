c45425384c91243520c17db3644838fa3e1f7a4a
myunpe <myunpe2@gmail.com>
2014-07-09 23:19:46 +0900

    GUI画面を更新しました。

--- UnityProject/Assets/UnityChan/Scripts/FaceUpdate.cs
+++ UnityProject/Assets/UnityChan/Scripts/FaceUpdate.cs
@@ -19,11 +19,11 @@ public class FaceUpdate : MonoBehaviour
 
 	void OnGUI ()
 	{
-		foreach (var animation in animations) {
-			if (GUILayout.Button (animation.name)) {
-				anim.CrossFade (animation.name, 0);
-			}
-		}
+//		foreach (var animation in animations) {
+//			if (GUILayout.Button (animation.name)) {
+//				anim.CrossFade (animation.name, 0);
+//			}
+//		}
 	}
 
 	float current = 0;
